732de9d632ed05b9cf8cdd48bf4ada647c2e1feb
abrar hussain <theibrar1000@gmail.com>
2024-05-05 03:09:12 -0700

    implement code push

--- android/app/src/main/java/com/ignitevanilla/MainApplication.java
+++ android/app/src/main/java/com/ignitevanilla/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -19,6 +20,11 @@ public class MainApplication extends Application implements ReactApplication {
           return BuildConfig.DEBUG;
         }
 
+          @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
+            }
+        
         @Override
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
@@ -42,6 +48,8 @@ public class MainApplication extends Application implements ReactApplication {
         protected Boolean isHermesEnabled() {
           return BuildConfig.IS_HERMES_ENABLED;
         }
+
+       
       };
 
   @Override
